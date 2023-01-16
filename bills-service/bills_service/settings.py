670cf046b6b0b62d8bde2c4c1fe8a02af56fa020
Alfredo Viera <alvie97m@gmail.com>
2023-01-16 20:00:59 -0300

    adds configuration files

--- bills-service/bills_service/settings.py
+++ bills-service/bills_service/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party apps
+    'django_restframework',
+
+    # own apps
+    'bills'
 ]
 
 MIDDLEWARE = [
