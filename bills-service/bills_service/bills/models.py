4ba806fc2ec8e5bd75bed3b45fa134c7a7ff4dd0
Alfredo Viera <alvie97m@gmail.com>
2023-01-28 11:21:05 -0300

    makes models

--- bills-service/bills_service/bills/models.py
+++ bills-service/bills_service/bills/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Bill(models.Model):
+    """
+    A bill is a statement of the money owned by a user for an expense.
+    """
+
+    user_id = models.CharField(max_length=36, blank=False, unique=True)
+    title = models.CharField(max_length=32, blank=False, unique=False)
+    description = models.CharField(max_length=256, blank=True, unique=False)
+    expense = models.DecimalField(max_digits=9, decimal_places=2, null=False)
